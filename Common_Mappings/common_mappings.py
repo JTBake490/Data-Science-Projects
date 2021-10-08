64e0dbd536eb550095a2d75aaed8f35a6888076a
James <41299731+JTBake490@users.noreply.github.com>
2021-10-08 17:19:00 -0400

    Punctuations and Country updates
    Added punctuation mappings and updated country codes replacement to handle Countries not in a mapping

--- Common_Mappings/common_mappings.py
+++ Common_Mappings/common_mappings.py
@@ -1,22 +1,28 @@
 import json
-import pathlib
-import Mappings.country_codes as country_codes
+from string import punctuation
+from Mappings import country_codes
 
 with open('Mappings/accents.json', 'r') as accent_file:
-    no_accents = json.load(accent_file)
+    no_accents_map = json.load(accent_file)
+    accent_table = str.maketrans(no_accents_map)
+    del no_accents_map
 
 with open('Mappings/directions.json', 'r') as direct_file:
     direct_map = json.load(direct_file)
+    direct_map = {key.upper() : value for key, value in direct_map.items()}
 
-def remove_accents(word, accents_map=no_accents):
+def remove_accents(word, accents_table=accent_table):
     '''Remove all accents from a character within a string if there is an ascii equivalent'''
-    table = str.maketrans(accents_map)
-    return word.translate(table)
+    return word.translate(accents_table)
+
+def remove_punctuation(text, punctuation=punctuation, sep=''):
+    '''Remove punctuation from a given text. Punctuation is provided from the string library.'''
+    no_punctuation = str.maketrans(dict.fromkeys(punctuation, sep))
+    return text.translate(no_punctuation).strip()
 
 def abbrev_directions(direction, direction_map=direct_map):
     '''Replace directions with a common abbreviated form'''
     result = []
-    direction_map = {key.upper() : value for key, value in direction_map.items()}
 
     for word in direction.split():
         upper_word = word.upper()
@@ -32,14 +38,17 @@ def abbrev_country(country, abbrev='ISO2', country_map=country_codes.countries):
     Converts a given country to an abbreviated form
     abbrev : {'ISO2', 'ISO3', 'UN_CODE'}, default='ISO2'
     '''
-    country = country.upper()
+    country_up = country.upper()
     abbrev = abbrev.upper()
 
     assert abbrev in ('ISO2', 'ISO3', 'UN_CODE'), 'abbrev must be ISO2, ISO3, or UN_CODE'
 
-    if abbrev == 'ISO2':
-        return country_map[country].ISO2
-    elif abbrev == 'ISO3':
-        return country_map[country].ISO3
-    elif abbrev == 'UN_CODE':
-        return country_map[country].UN_CODE
+    try:
+        if abbrev == 'ISO2':
+            return country_map[country_up].ISO2
+        elif abbrev == 'ISO3':
+            return country_map[country_up].ISO3
+        elif abbrev == 'UN_CODE':
+            return country_map[country_up].UN_CODE
+    except KeyError:
+        return country
